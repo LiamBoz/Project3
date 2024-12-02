3bab6ada33f2b54f02073ae39c2e8bedf5ff78f9
liam <thisamazingnow@gmail.com>
2024-12-02 17:01:27 -0500

    ITS ALMOST THERE... DFS WORKS FOR MAZE GEN BUT BACKTRACKING NOT FUNCTIONAL

--- main.cpp
+++ main.cpp
@@ -3,28 +3,49 @@
 #include <sstream>
 #include <cstdlib>
 #include <stack>
+#include <random>
 
 using namespace std;
 
 // maze size, will be changed later but is 20x20 for ease of use
-const int width = 20;
-const int height = 20;
+const int width = 50;
+const int height = 50;
 
-bool visitableCell(int x, int y, const vector<vector<int>>& maze) {
+// directions to move in
+vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+// random num gen credit: https://stackoverflow.com/questions/7560114/random-number-c-in-some-range
+//
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> distr(0, 3);
+
+// VISITABLE CELL must check 3 co
+bool visitableCell(int currX, int currY, const vector<vector<int>>& maze) {
   //make sure the DFS doesn't run into a wall
-    if (x <= 0 || x >= width - 1 || y <= 0 || y >= height - 1) {
+    if (currX <= 0 || currX >= width - 1 || currY <= 0 || currY >= height - 1) {
         return false;
     }
-  //make sure the cell is surrounded by walls
+    int numWalls = 0;
+    //make sure the cell is surrounded by walls
+    for (int i = 0; i < 4; i++) {
+      int newX = currX + directions[i].first; 
+      int newY = currY + directions[i].second;
+
+      if (maze[newX][newY] == 0 && newX >= 0 && newX < width && newY >= 0 && newY < height) {
+        numWalls++;
+      }
+    }
+    if (numWalls >= 3) {
+      return true;
+    }
+    else {
+      return false;
+    }
 
 }
 
-
-
-
-
-
-string generatemaze() {
+string generateMaze() {
 
     //TODO: loop to continue while stack is not empty,
     // pop the top of the stack and check if it is a dead end, if it is,
@@ -33,12 +54,6 @@ string generatemaze() {
 
     // 0 is a wall, 1 is a path
     vector<vector<int>> maze(width, vector<int>(height, 0));
-
-
-
-    // directions to move in
-    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
     // stringstream to print the maze
     stringstream ss;
 
@@ -55,12 +70,48 @@ string generatemaze() {
     maze[width - 1][middleY] = 1;
 
     // stack to keep track of current position
-    stack<pair<int, int>> currPosStack;
+    stack<pair<int, int>> currPos;
     pair<int, int> startCell = {0, middleY};
-    currPosStack.push(startCell);
-    
-    while (!currPosStack.empty()) {
-      currPosStack.pop();
+    currPos.push(startCell);
+   
+    // Choose a random direction. Call visitableCell() on the cell in that direction. 
+    // If visitableCell returns true, then we add it to the stack, remove the wall and the while loop repeats.
+    // It becomes our new currPosStack.
+    // If it doesn't, we choose a different random direction & call visitableCell on it.
+    // We do this until
+    //
+    while (!currPos.empty()) {
+      int currX = currPos.top().first;
+      int currY = currPos.top().second;
+
+      currPos.pop();
+
+      bool directions_guessed[4] = {false, false, false, false};
+      bool allTried = false;
+
+      while (!allTried) {
+        int dirIndex = distr(gen);
+
+        directions_guessed[dirIndex] = true;
+
+        int newX = currX + directions[dirIndex].first;
+        int newY = currY + directions[dirIndex].second;
+
+        if (visitableCell(newX, newY, maze)) {
+          maze[newX][newY] = 1;
+
+          currPos.push({newX, newY});
+          break;
+        }
+        
+        allTried = true;
+        for (int i = 0; i < 4; i++) {
+          if (!directions_guessed[i]) {
+            allTried = false;
+            break;
+          }
+        }
+      }
     }
 
 
@@ -84,7 +135,7 @@ string generatemaze() {
 
 
 int main() {
-    string maze = generatemaze();
+    string maze = generateMaze();
     cout << maze;
 
     return 0;
